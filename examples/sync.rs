94b115f33db8ab4b22fa944e051433380c09893b
Al Liu <scygliu1@gmail.com>
2023-10-04 04:08:17 +0800

    Initial commit

---
+++ examples/sync.rs
@@ -0,0 +1,26 @@
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Arc;
+use std::thread::{sleep, spawn};
+use std::time::Duration;
+use wg::WaitGroup;
+
+fn main() {
+    let wg = WaitGroup::new();
+    let ctr = Arc::new(AtomicUsize::new(0));
+
+    for _ in 0..5 {
+        let ctrx = ctr.clone();
+        let t_wg = wg.add(1);
+        spawn(move || {
+            // mock some time consuming task
+            sleep(Duration::from_millis(50));
+            ctrx.fetch_add(1, Ordering::Relaxed);
+
+            // mock task is finished
+            t_wg.done();
+        });
+    }
+
+    wg.wait();
+    assert_eq!(ctr.load(Ordering::Relaxed), 5);
+}
