7415127c81b3ca9d9599290e8c7b3bfd19d34723
al8n <scygliu1@gmail.com>
2025-11-04 20:58:41 +1300

    cleanup

--- src/address.rs
+++ src/address.rs
@@ -3,11 +3,13 @@ use core::fmt::{Debug, Display};
 use cheap_clone::CheapClone;
 
 /// Host address type alias
-#[cfg(any(feature = "std", feature = "alloc"))]
+#[cfg(feature = "hostaddr")]
+#[cfg_attr(docsrs, doc(cfg(feature = "hostaddr")))]
 pub type HostAddr = hostaddr::HostAddr<smol_str_0_3::SmolStr>;
 
 /// Domain type alias
-#[cfg(any(feature = "std", feature = "alloc"))]
+#[cfg(feature = "hostaddr")]
+#[cfg_attr(docsrs, doc(cfg(feature = "hostaddr")))]
 pub type Domain = hostaddr::Domain<smol_str_0_3::SmolStr>;
 
 /// Address abstraction for distributed systems
