1d72f8f6b8175ae3f835596006da872673437a5f
al8n <scygliu1@gmail.com>
2025-11-04 23:52:32 +1300

    chore: add type aliases and re-export hostaddr crate

--- src/address.rs
+++ src/address.rs
@@ -12,6 +12,20 @@ pub type HostAddr = hostaddr::HostAddr<smol_str_0_3::SmolStr>;
 #[cfg_attr(docsrs, doc(cfg(feature = "hostaddr")))]
 pub type Domain = hostaddr::Domain<smol_str_0_3::SmolStr>;
 
+/// Host address type alias
+#[cfg(feature = "hostaddr")]
+#[cfg_attr(docsrs, doc(cfg(feature = "hostaddr")))]
+pub type HostAddrBuffer = hostaddr::HostAddr<hostaddr::Buffer>;
+
+/// Domain type alias
+#[cfg(feature = "hostaddr")]
+#[cfg_attr(docsrs, doc(cfg(feature = "hostaddr")))]
+pub type DomainBuffer = hostaddr::Domain<hostaddr::Buffer>;
+
+#[cfg(feature = "hostaddr")]
+#[cfg_attr(docsrs, doc(cfg(feature = "hostaddr")))]
+pub use hostaddr;
+
 /// Address abstraction for distributed systems
 pub trait Address:
   CheapClone + Eq + Ord + core::hash::Hash + Debug + Display + Sized + Unpin + 'static
